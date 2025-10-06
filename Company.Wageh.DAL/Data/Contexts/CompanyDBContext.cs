e893ec5f60ad87ad62abbfdeb4dd6d05e6627368
Wageh_Mostafa <120650371+wageh3@users.noreply.github.com>
2025-10-06 18:58:47 +0300

    implemntation of DAL

---
+++ Company.Wageh.DAL/Data/Contexts/CompanyDBContext.cs
@@ -0,0 +1,31 @@
+﻿using Company.Wageh.DAL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Company.Wageh.DAL.Data.Contexts
+{
+    internal class CompanyDBContext : DbContext
+    {
+        public CompanyDBContext(DbContextOptions<CompanyDBContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("Server=.;Database=Company.Mostafa;Trusted_Connection=True;TrustServerCertificate=True;");
+        //}
+
+        public DbSet<Department> Departments { get; set; }
+    }
+}
