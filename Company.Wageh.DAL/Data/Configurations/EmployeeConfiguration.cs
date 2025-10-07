a707e9cd81dd8ef0e97c3e28f07a684f55fc9593
Wageh_Mostafa <120650371+wageh3@users.noreply.github.com>
2025-10-07 17:43:52 +0300

    Implmentation od DAL For Employee Service

---
+++ Company.Wageh.DAL/Data/Configurations/EmployeeConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Company.Wageh.DAL.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Wageh.DAL.Data.Configurations
+{
+    internal class EmployeeConfiguration : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+            builder.Property(E=>E.Salary).HasColumnType("decimal(18,2)");
+        }
+    }
+}
