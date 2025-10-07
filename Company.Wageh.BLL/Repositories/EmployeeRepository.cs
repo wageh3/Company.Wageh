30c0a904b8aa1ac48d613dd5e9ea2bea6463f00f
Wageh_Mostafa <120650371+wageh3@users.noreply.github.com>
2025-10-07 17:58:49 +0300

    Implmentation of BLL for Employee Service

---
+++ Company.Wageh.BLL/Repositories/EmployeeRepository.cs
@@ -0,0 +1,48 @@
+﻿using Company.Wageh.BLL.Interfaces;
+using Company.Wageh.DAL.Data.Contexts;
+using Company.Wageh.DAL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Wageh.BLL.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly CompanyDBContext _context;
+
+        public EmployeeRepository(CompanyDBContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Employee> GetAll()
+        {
+            return _context.Employees.ToList();
+        }
+
+        public Employee? Get(int id)
+        {
+            return _context.Employees.Find(id);
+        }
+
+        public int Add(Employee model)
+        {
+            _context.Employees.Add(model);
+            return _context.SaveChanges();
+        }
+
+        public int Delete(Employee model)
+        {
+            _context.Employees.Remove(model);
+            return _context.SaveChanges();
+        }
+
+        public int Update(Employee model)
+        {
+            _context.Employees.Update(model);
+            return _context.SaveChanges();
+        }
+    }
+}
