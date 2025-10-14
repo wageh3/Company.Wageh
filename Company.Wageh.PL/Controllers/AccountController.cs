30ec0476ecbf94d2fc680856aaef13f5aff1b712
Wageh_Mostafa <120650371+wageh3@users.noreply.github.com>
2025-10-14 15:24:33 +0300

    Forget And Reset Password

--- Company.Wageh.PL/Controllers/AccountController.cs
+++ Company.Wageh.PL/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 ﻿using Company.Wageh.DAL.Model;
 using Company.Wageh.PL.Dto;
+using Company.Wageh.PL.Helpers;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
@@ -89,8 +90,87 @@ namespace Company.Wageh.PL.Controllers
         }
         #endregion
 
-        #region SignOut
+        #region Forget Password
+        [HttpGet]
+        public IActionResult ForgetPassword()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> ForgetPassword(ForgetPasswordDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(model.Email);
+                if (user is not null) 
+                {
+                    //Generate Token
+                    var token = await _userManager.GeneratePasswordResetTokenAsync(user);
+                    //Generate URL
+                    var url = Url.Action("ResetPassword","Account",new {email = model.Email , token},Request.Scheme);
+                    //Generate Email
+                    var email = new Email()
+                    {
+                        To = model.Email,
+                        Subject = "Reset Password",
+                        Body = url
+                    };
+                    //Send Email
+                   var flag = EmailSettings.SendEmail(email);
+                    if (flag)
+                    {
+                        return RedirectToAction("CheckYourInbox");
+                    }
+                }
+            }
+            ModelState.AddModelError("","Invalid ForgetPassword Operation !!");
+            return View("ForgetPassword" ,model);
+        }
 
+        [HttpGet]
+        public IActionResult CheckYourInbox()
+        {
+            return View();
+        }
+        #endregion
+
+        #region Reset Password
+        [HttpGet]
+        public IActionResult ResetPassword(string email , string token)
+        {
+            TempData["email"] =email;
+            TempData["token"] =token;
+            return View();
+        } 
+        [HttpPost]
+        public async Task<IActionResult> ResetPassword(ResetPasswordDto model)
+        {
+            if (ModelState.IsValid) 
+            {
+                var email = TempData["email"] as string;
+                var token = TempData["token"] as string;
+                if (email is null || token is null ) return BadRequest("Invalid Operation");
+                var user = await _userManager.FindByEmailAsync(email);
+                if (user is not null)
+                {
+                    var result = await _userManager.ResetPasswordAsync(user,token, model.NewPassword);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("SignIn");
+                    }
+                }
+                ModelState.AddModelError("", "Invalid Reset Password Operation");
+            }
+            
+            return View();
+        }
+        #endregion
+        #region SignOut
+        public async Task<IActionResult> SignOut()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("SignIn");
+        }
         #endregion
     }
 }
---
+++ Company.Wageh.PL/Helpers/EmailSettings.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing.Drawing2D;
+using System.Net;
+using System.Net.Mail;
+
+namespace Company.Wageh.PL.Helpers
+{
+    public class EmailSettings
+    {
+        public static bool SendEmail(Email email)
+        {
+            //Mail Server : Gmail
+
+            //SMTP
+            try 
+            {
+                var client = new SmtpClient("smtp.gmail.com", 587);
+                client.EnableSsl = true;
+                client.Credentials = new NetworkCredential("wagehmostafadeveloper@gmail.com", "xjtatkzdpxsihnuu");
+                client.Send("wagehmostafadeveloper@gmail.com", email.To, email.Subject, email.Body);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
